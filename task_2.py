845457931823b7d9711ddcb3c95a5664171197ce
DianaKravtsova <di.kravts@mail.ru>
2020-03-04 10:16:10 +0300

    second

--- task_2.py
+++ task_2.py
@@ -4,26 +4,30 @@ class Leibniz:
     def __iter__(self):
         return self
 
-    def __init__(self):
+    def __init__(self, SUM):
         self.n = 1
-        self.sum = 1.76
-        self.sum0 = 1
-        self.sum_leib = (-pow(1,self.n))/ (2*self.n + 1)
+        self.sum = SUM
+        self.sum0 = 0
+        self.sum_leib = (-pow(1,self.n)) / (2*self.n + 1)
+        self.i = 1
 
 
     def __next__(self):
 
-        if round(fabs(self.sum - self.sum0), 2) < 0.01:
-            self.sum0 += self.sum_leib
-            self.n +=1
-
+        if round(fabs(self.sum0 - self.sum), 2) > 0.01:
+            self.sum0 += self.i
+            self.i = self.sum_leib
+            self.n += 1
+            self.sum_leib = (pow(-1, self.n)) / (2 * self.n + 1)
+            return self.i
         else:
             raise StopIteration
 
 
 
-
-row = Leibniz()
+sum = float(input("Ожидаемая сумма:", ))
+print("Полученный ряд:")
+row = Leibniz(sum)
 for i in row:
     print(i)
 
